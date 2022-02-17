e43a4b15a0b8292941298d4f02b567a2af3c26f8
Rifat Shikder <rifatptk@gmail.com>
2022-02-17 19:45:12 +0600

    added javascript file

---
+++ main.js
@@ -0,0 +1,7 @@
+// let container=document.querySelector(".navlinks-container");
+// let btn=document.querySelector(".navbtn");
+// function toggle(){
+// btn.classList.toggle("clicked");
+// container.classList.toggle("mobnav");
+ 
+// }
