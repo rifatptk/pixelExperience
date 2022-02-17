abff9e4228ebf86f57d3622c10b16289aabe0251
Rifat Shikder <rifatptk@gmail.com>
2022-02-17 19:47:03 +0600

    added mobile responsive navigation menu

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-// let container=document.querySelector(".navlinks-container");
-// let btn=document.querySelector(".navbtn");
-// function toggle(){
-// btn.classList.toggle("clicked");
-// container.classList.toggle("mobnav");
- 
-// }
+let container = document.querySelector(".navlinks-container");
+let btn = document.querySelector(".navbtn");
+
+
+function toggle(){
+btn.classList.toggle("clicked");
+container.classList.toggle("mobnav");
+}
